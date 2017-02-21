02622ab8131514c3df8236c1771cf48fee15808c
uriel77 <uriel_m_cor@hotmail.com>
2017-02-21 13:10:14 -0800

    se declararon los elementos de la interfaz aun se sigue en la clase ParseJSONObject

---
+++ app/src/main/java/com/example/uriel/jsonparser/ParseJSONObject.java
@@ -0,0 +1,30 @@
+package com.example.uriel.jsonparser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by uriel on 2/21/2017.
+ */
+
+public class ParseJSONObject extends AppCompatActivity {
+    private static final String TAG="ParseJSONObject";
+    private final String EXTRA_JSON_OBJECT_INDEX = "com.androidtutorialpoint.jsonparser";
+
+    private Mobile mMobile;
+    private TextView nameTextView;
+    private TextView companyNameTextView;
+    private TextView operatingSystemTextView;
+    private TextView processorTextView;
+    private TextView ramTextView;
+    private TextView romTextView;
+    private TextView frontCameraTextView;
+    private TextView backCameraTextView;
+    private TextView screenSizeTextView;
+    private TextView batteryTextView;
+    private ImageView photoImageView;
+    private String photoUrl;
+
+    String url = "https://androidtutorialpoint.com/api/MobileJSONObject.json";
+}
